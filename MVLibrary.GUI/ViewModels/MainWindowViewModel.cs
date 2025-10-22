0bb3b98586b90aa39caca4b4e368375f93d4043a
RyoTune <zutomayofan50@gmail.com>
2025-10-22 06:00:10 -0500

    Add GUI project.

---
+++ MVLibrary.GUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace MVLibrary.GUI.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ MVLibrary.GUI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace MVLibrary.GUI.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
