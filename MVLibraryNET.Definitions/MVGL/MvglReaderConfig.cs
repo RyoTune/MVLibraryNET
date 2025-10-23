6e65f90d2f1856c940957c778d7e6e96e5872959
RyoTune <zutomayofan50@gmail.com>
2025-10-23 12:37:37 -0500

    Add docs.

--- MVLibraryNET.Definitions/MVGL/MvglReaderConfig.cs
+++ MVLibraryNET.Definitions/MVGL/MvglReaderConfig.cs
@@ -1,6 +1,12 @@
 namespace MVLibraryNET.Definitions.MVGL;
 
+/// <summary>
+/// MVGL reader config.
+/// </summary>
 public class MvglReaderConfig
 {
+    /// <summary>
+    /// Function to normalize MVGL file names with.
+    /// </summary>
     public Func<string, string>? FileNameNormalizer { get; init; }
 }
